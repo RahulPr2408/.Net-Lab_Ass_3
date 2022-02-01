6be5c6f3c0a0ce9c9f521d4e0082cc6e1120a702
Rahul <prajapati.rahu201@gmail.com>
2022-02-01 21:45:55 +0530

    lab_ass_3

---
+++ Code_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Code_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerator; 
+            int denominator;
+
+            Console.Write("Please enter the numerator : ");
+            numerator = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the denominato : ");
+            denominator = int.Parse(Console.ReadLine());
+
+            int quotient = numerator / denominator;
+            int remainder = numerator % denominator;
+
+            Console.WriteLine($"Integer division result = {quotient} with a remainder = {remainder}");
+
+            double fraction_quotient = Math.Round((double)numerator/denominator);
+
+            Console.WriteLine($"Floating point division result : {fraction_quotient}");
+
+            Console.WriteLine($"The result of mixed fraction : {quotient} {remainder}/{denominator}");
+
+
+        }
+    }
+}
---
+++ Code_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Code_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // find length of a String
+            Console.Write("Enter a string to find Length : ");
+            String str1 = Console.ReadLine();
+
+            int length = str1.Length;
+            Console.WriteLine($"The length of {str1} = {length}");
+
+            // check sentences
+            String str2; 
+            Console.Write("Enter a Sentence : ");
+            str2 = Console.ReadLine();
+            checkSentence(str2);
+
+            // full name
+            String str3;
+            Console.Write("Enter Your Full name (without Middle name) : ");
+            str3 = Console.ReadLine();
+            FullName(str3);
+        }
+
+        public static void checkSentence(String str) {
+            if(str.EndsWith(".")) {
+                Console.WriteLine("The sentence is Declarative");
+            }
+            else if (str.EndsWith("?")) {
+                Console.WriteLine("The sentence is Interogatory");
+            }
+            else if (str.EndsWith("!")) {
+                Console.WriteLine("The sentence is Exclaimatory");
+            } 
+            else {
+                Console.WriteLine("This is not a sentence ");
+            }
+        }
+
+        public static void FullName(String str) {
+            int index = str.IndexOf(" ");
+
+            String firstName = str.Substring(0,index);
+            String lastName = str.Substring(index);
+
+            Console.WriteLine($"{lastName} , {firstName}");
+        } 
+    }
+}
